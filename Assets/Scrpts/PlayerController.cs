b1a00762fc76d65655cd0807915d03091d269506
alvaro2302 <alvaro230297@hotmail.com>
2021-02-27 14:37:26 -0400

    add jump

--- Assets/Scrpts/PlayerController.cs
+++ Assets/Scrpts/PlayerController.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float forceJump = 6f;
+    private Rigidbody2D rigidbodyPlayer;
+    void Awake()
+    {
+        rigidbodyPlayer = GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -13,6 +19,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+
+            Jump();
+
+        }
+    }
+
+    void Jump()
+    {
+
+        rigidbodyPlayer.AddForce(Vector2.up * forceJump, ForceMode2D.Impulse);
     }
 }
